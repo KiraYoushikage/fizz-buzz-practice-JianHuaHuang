252ed7b57b32aa4c1f445cc945a47e76483f849a
KiraYous <404465394@qq.com>
2022-07-21 14:45:31 +0800

    feat: add FIZZ when given 3;pass the test01

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,7 +2,14 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
 
+    public static final String FIZZ="Fizz";
+
     public String countOff(int number){
-        return "";
+
+        String res="";
+        if(number%3==0){
+            res+=FIZZ;
+        }
+        return res;
     }
 }
